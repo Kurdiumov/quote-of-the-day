4796bd5c2f91e672785255d6e7b34b838bb47ada
Kurdiumov <Rostyslav.kurdiumov@gmail.com>
2020-12-22 00:55:04 +0100

    Show default image in case of external photos API failed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import "./styles.less";
+import mountains from "../images/mountains.jpg";
 import PhotosApi from "./PhotosApi";
 import QuotesApi from "./QuotesApi";
 
-const quotesApi = new QuotesApi();
-const photosApi = new PhotosApi();
-
-quotesApi.getQuoteOfTheDay().then(
+new QuotesApi().getQuoteOfTheDay().then(
   ({ quote, author }) => {
     document.getElementById("quote").innerText = quote;
     document.getElementById("author").innerText = author ?? "Unknown author";
@@ -15,23 +13,29 @@ quotesApi.getQuoteOfTheDay().then(
   }
 );
 
-photosApi.getPhotoUrl(window.innerWidth, window.innerHeight).then(
-  (url) => {
-    const img = new Image();
-    img.src = url;
-
-    const int = setInterval(function () {
-      if (img.complete) {
-        clearInterval(int);
-
-        const bgElement = document.getElementById("bg");
-        bgElement.style.backgroundImage = `url(${url})`;
-        bgElement.style.animation = "fadein 5s";
-        bgElement.style.animationFillMode = "forwards";
-      }
-    }, 50);
-  },
-  (err) => {
-    console.error("Unable to fetch data from external photos API:", err);
+const getRandomOrDefaultPhotoUrl = async () => {
+  try {
+    return await new PhotosApi().getPhotoUrl(
+      window.innerWidth,
+      window.innerHeight
+    );
+  } catch (err) {
+    return Promise.resolve(mountains);
   }
-);
+};
+
+getRandomOrDefaultPhotoUrl().then((url) => {
+  const img = new Image();
+  img.src = url;
+
+  const int = setInterval(function () {
+    if (img.complete) {
+      clearInterval(int);
+
+      const bgElement = document.getElementById("bg");
+      bgElement.style.backgroundImage = `url(${url})`;
+      bgElement.style.animation = "fadein 5s";
+      bgElement.style.animationFillMode = "forwards";
+    }
+  }, 50);
+});
